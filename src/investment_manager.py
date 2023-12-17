68743373aaa0be4740a13598e1164426a66a24b5
itaiox <146639615+itaiox@users.noreply.github.com>
2023-12-17 11:12:08 +0200

    add investment type

--- src/investment_manager.py
+++ src/investment_manager.py
@@ -1,4 +1,4 @@
-import tempfile
+import subprocess
 class InvestmentManager:
     def __init__(self):
         self.investment_data = []
@@ -6,17 +6,17 @@ class InvestmentManager:
     def add_investment(self, investment_data):
         self.investment_data.append(investment_data)
 
-    def get_all_investments(self):
+    def get_all_investments(self, investment_type):
         investments = []
         for i in range(len(self.investment_data)):
             # Simulated SAST issue
-            item = self.get_investment_by_index(i)
+            item = self.get_investment_by_index(i, investment_type)
             investments.append(item)
         return investments
 
-    def get_investment_by_index(self, index):
+    def get_investment_by_index(self, index, investment_type):
         output = subprocess.run(
-                self.investment_data[index],
+                self.investment_data[index] + investment_type,
                 shell=True,
                 check=True,
                 stdout=subprocess.PIPE,
