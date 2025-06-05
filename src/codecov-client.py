5bc37b036a0246e4aba878a29fd77f8b81310360
ruslanOX <ruslan@ox.security>
2025-06-05 15:53:28 +0400

    Update codecov-client.py

--- src/codecov-client.py
+++ src/codecov-client.py
@@ -2,18 +2,3 @@ from collections import defaultdict
 import json
 import requests
 
-TOKEN_NAME = "{{ ACCESS TOKEN }}"
-CODECOV_HEADERS = {
-    'Authorization': 'bearer {}'.format(TOKEN_NAME)
-}
-
-def _get_all_repos():
-    print('Retrieving all repos...', end=" ")
-    endpoint = CODECOV_ENDPOINT.format('repos?page_size=500')
-    response = requests.get(
-        endpoint,
-        headers=CODECOV_HEADERS,
-    )
-    repos = json.loads(response.content)['results']
-    print("Found {} repositories".format(len(repos)))
-    return sorted(repo['name'] for repo in repos, key=str.casefold)
